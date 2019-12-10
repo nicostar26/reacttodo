8fff5ff907272877c84fb99a3b47ddd5a8a547c4
Nicole Saunders <nicostar26@yahoo.com>
2019-12-10 10:51:47 -0500

    Initial commit

---
+++ src/components/Todoitem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Todoitem extends Component {
+    render() {
+        return (
+            <div>
+             <p>{this.props.todo.title}</p>
+                
+            </div>
+        )
+    }
+}
+
+export default Todoitem
